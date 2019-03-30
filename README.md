963caf7966dd2e127ec4e070a82aa3dca8f01a58
Stephen <sheldon2521@gmail.com>
2019-03-30 11:34:06 -0400

    Initial Commit. Refactoring giflib to implement Hibernate ORM

---
+++ README.md
@@ -0,0 +1,12 @@
+What this app does:
+
+- Serves dynamic web content according to URI, including index and detail pages for categories and GIFs
+- Includes database connectivity, where GIF data is stored
+- Allows a user to perform CRUD (create, read, update, delete) operations on GIF and category data
+- Performs server-side form validation for adding/updating GIFs and categories
+- Uses a database
+- Serves static assets, such as images, fonts, CSS, and JS
+
+What this app does **NOT** do:
+
+- Implement user authentication
---
+++ src/main/java/com/ssheld/giflib/model/Category.java
@@ -0,0 +1,48 @@
+package com.ssheld.giflib.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String colorCode;
+
+    @OneToMany(mappedBy = "category")
+    private List<Gif> gifs = new ArrayList<>();
+
+    public Category(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public List<Gif> getGifs() {
+        return gifs;
+    }
+
+}
